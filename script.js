ac506ebbc8a9d806d04c3425a657197892c3eb23
Samyra Figueiredo <samy_franca@outlook.com>
2022-07-05 16:56:21 +0100

    site mobile e tablet

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 $(document).ready(() => {
     $('#anaProfile').removeClass('d-none');
+    $('#anaProfile2').removeClass('d-none');
     $('#matheusProfile').addClass('d-none');
+    $('#matheusProfile2').addClass('d-none');
     $('#claudiaProfile').addClass('d-none');
+    $('#claudiaProfile2').addClass('d-none');
     second()
 })
 
 function first() {
     setTimeout(() => {
         $('#anaProfile').removeClass('d-none');
+        $('#anaProfile2').removeClass('d-none');
         $('#matheusProfile').addClass('d-none');
+        $('#matheusProfile2').addClass('d-none');
         $('#claudiaProfile').addClass('d-none');
+        $('#claudiaProfile2').addClass('d-none');
         second()
     }, 3000);
 }
@@ -17,8 +23,11 @@ function first() {
 function second() {
     setTimeout(() => {
         $('#matheusProfile').removeClass('d-none');
+        $('#matheusProfile2').removeClass('d-none');
         $('#anaProfile').addClass('d-none');
+        $('#anaProfile2').addClass('d-none');
         $('#claudiaProfile').addClass('d-none');
+        $('#claudiaProfile2').addClass('d-none');
         third()
     }, 3000);
 }
@@ -26,8 +35,11 @@ function second() {
 function third() {
     setTimeout(() => {
         $('#claudiaProfile').removeClass('d-none');
+        $('#claudiaProfile2').removeClass('d-none');
         $('#anaProfile').addClass('d-none');
+        $('#anaProfile2').addClass('d-none');
         $('#matheusProfile').addClass('d-none');
+        $('#matheusProfile2').addClass('d-none');
         first()
     }, 3000);
 }
